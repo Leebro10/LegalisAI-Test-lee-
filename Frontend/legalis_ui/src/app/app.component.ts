7f26308d1c732c6acfa9439596f4256adfbd8ca4
Aarol D'Souza <aarol.dsouza@gmail.com>
2025-02-17 18:48:22 +0530

    Features Component Gen, to be added

--- Frontend/legalis_ui/src/app/app.component.ts
+++ Frontend/legalis_ui/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './pages/navbar/navbar.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { FeaturesComponent } from './pages/features/features.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,FeaturesComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/legalis_ui/src/app/pages/home/home.component.ts
+++ Frontend/legalis_ui/src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterLink } from '@angular/router';
 import { TypebannerComponent } from '../typebanner/typebanner.component';
+import { FeaturesComponent } from '../features/features.component';
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [NavbarComponent,TypebannerComponent],  //Add imports here
+  imports: [NavbarComponent,FeaturesComponent,TypebannerComponent,],  //Add imports here
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
