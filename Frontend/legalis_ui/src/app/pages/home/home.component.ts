cd9dbb0458f666c1ee4e8f0863e7bf36b4f5802b
Aarol D'Souza <aarol.dsouza@gmail.com>
2025-02-26 11:31:12 +0530

    Added Features Section

--- Frontend/legalis_ui/src/app/pages/home/home.component.ts
+++ Frontend/legalis_ui/src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterLink } from '@angular/router';
 import { TypebannerComponent } from '../typebanner/typebanner.component';
 import { FeaturesComponent } from '../features/features.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [NavbarComponent,FeaturesComponent,TypebannerComponent,],  //Add imports here
+  imports: [NavbarComponent,FeaturesComponent,TypebannerComponent,FooterComponent,],  //Add imports here
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
